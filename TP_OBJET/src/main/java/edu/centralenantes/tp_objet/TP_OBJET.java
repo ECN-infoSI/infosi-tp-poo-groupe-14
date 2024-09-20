ce16b0def23965cb4c3257528a4502a297407728
samer <samer@130.66.204.115>
2024-09-20 09:48:30 +0200

    test git

---
+++ TP_OBJET/src/main/java/edu/centralenantes/tp_objet/TP_OBJET.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.centralenantes.tp_objet;
+
+/**
+ *
+ * @author samer
+ */
+public class TP_OBJET {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
